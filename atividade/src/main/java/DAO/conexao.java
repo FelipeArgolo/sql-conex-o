12922916d1d976c237f1ce7062ea940d2fee7ef4
Felipe <143637558+FelipeArgolo@users.noreply.github.com>
2024-11-05 16:49:53 -0300

    Add files via upload

---
+++ atividade/src/main/java/DAO/conexao.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class conexao {
+
+    public Connection conectabd() {
+    
+        Connection conn = null;
+     
+        try {
+            String url = "jdbc:mysql://localhost/banco?user=root&password";
+            conn = DriverManager.getConnection(url);
+        
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        
+        }
+        return conn;
+}
+
+}
